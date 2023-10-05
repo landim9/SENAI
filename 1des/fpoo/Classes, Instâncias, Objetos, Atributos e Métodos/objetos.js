ae210f25ec38ab1dbc75f911a7f68ffb54d586fc
bryanbeckham06 <beckham.souza06@eaportal.org>
2023-10-05 11:27:45 -0300

    senai

---
+++ 1des/fpoo/Classes, Instâncias, Objetos, Atributos e Métodos/objetos.js
@@ -0,0 +1,123 @@
+//Objeto vazio, declarado diretamente
+const obj = {};
+//Acrescentando atributos dinamicamente
+obj.nome = 'Bola';
+obj.preço = 39.90;
+obj.qtd = 10;
+//Acrescentando métodos com function, dinânicamente
+obj.total = function () {
+    return obj.preço * obj.qtd;
+}
+//Acrescentando outro método com arrow function, dinamicamente
+obj.showHTML = () => {
+    let str = `<label>Nome:</label><label>${obj.nome}</label>`;
+    str += `<label>Preço:</label><label>${obj.preço}</label>`;
+    str += `<label>Quantidade:</label><label>${obj.qtd}</label>`;
+    str += `<label>Total:</label><label>${obj.total().toFixed(2)}</label>`;
+    return str;
+}
+
+//Objeto cheio, com atributos e métodos
+const obj2 = {
+    //Atributos locais do objeto
+    nome: "Mesa de escritório",
+    preco: 349.90,
+    qtd: 2,
+    //Métodos locais do objeto
+    total() {
+        return this.preco * this.qtd;
+    },
+    showHTML() {
+        let str = `<label>Nome:</label><label>${this.nome}</label>`;
+        str += `<label>Preço:</label><label>${this.preco}</label>`;
+        str += `<label>Quantidade:</label><label>${this.qtd}</label>`;
+        str += `<label>Total:</label><label>${this.total().toFixed(2)}</label>`;
+        return str;
+    }
+}
+
+//Terceiro objeto, com atributos e métodos
+const obj3 = {
+    //Atributos locais do objeto
+    nome: "Caneta azul",
+    preco: 2.99,
+    qtd: 10,
+    //Métodos locais do objeto
+    total() {
+        return this.preco * this.qtd;
+    },
+    showHTML() {
+        let str = `<label>Nome:</label><label>${this.nome}</label>`;
+        str += `<label>Preço:</label><label>${this.preco}</label>`;
+        str += `<label>Quantidade:</label><label>${this.qtd}</label>`;
+        str += `<label>Total:</label><label>${this.total().toFixed(2)}</label>`;
+        return str;
+    }
+}
+
+const obj4 = {
+    nome: "Celta Preto",
+    preco: 7349.90,
+    qtd: 2,
+    total() {
+        return this.preco * this.qtd;
+    },
+    showHTML() {
+        let str = `<label>Nome:</label><label>${this.nome}</label>`;
+        str += `<label>Preço:</label><label>${this.preco}</label>`;
+        str += `<label>Quantidade:</label><label>${this.qtd}</label>`;
+        str += `<label>Total:</label><label>${this.total().toFixed(2)}</label>`;
+        return str;
+    }
+}
+
+const obj5 = {
+    nome: "Ventilador",
+    preco: 149.90,
+    qtd: 2,
+    total() {
+        return this.preco * this.qtd;
+    },
+    showHTML() {
+        let str = `<label>Nome:</label><label>${this.nome}</label>`;
+        str += `<label>Preço:</label><label>${this.preco}</label>`;
+        str += `<label>Quantidade:</label><label>${this.qtd}</label>`;
+        str += `<label>Total:</label><label>${this.total().toFixed(2)}</label>`;
+        return str;
+    }
+}
+
+//Saídas no console
+console.log(obj);
+console.log('Valor total = R$ ' + obj.total().toFixed(2));
+console.log(obj2);
+console.log('Valor total = R$ ' + obj2.total().toFixed(2));
+console.log(obj3);
+console.log('Valor total = R$ ' + obj3.total().toFixed(2));
+console.log(obj4);
+console.log('Valor total = R$ ' + obj3.total().toFixed(2));
+console.log(obj5);
+console.log('Valor total = R$ ' + obj3.total().toFixed(2));
+
+//Saída no HTML
+const main = document.getElementById('objetos');
+
+const div = document.createElement('div');
+div.innerHTML = obj.showHTML();
+main.appendChild(div);
+
+const div2 = document.createElement('div');
+div2.innerHTML = obj2.showHTML();
+main.appendChild(div2);
+
+const div3 = document.createElement('div');
+div3.innerHTML = obj3.showHTML();
+main.appendChild(div3);
+
+const div4 = document.createElement('div');
+div4.innerHTML = obj4.showHTML();
+main.appendChild(div4);
+
+const div5 = document.createElement('div');
+div5.innerHTML = obj5.showHTML();
+main.appendChild(div5);
