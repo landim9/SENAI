360395a2e1a5c16edeb5761a5436078071b88974
landim9 <landimr9des@gmail.com>
2023-11-13 22:44:41 -0300

    senai

--- 1des/lima/portfolio/script.js
+++ 1des/lima/portfolio/script.js
@@ -1,6 +1,16 @@
-let popup = document.querySelector('.island')
-popup.onclick = function(){
-    popup.classList.toggle('active')
+const text = document.querySelector(".sec-text");
 
-}
+        const textLoad = () => {
+            setTimeout(() => {
+                text.textContent = "Freelancer";
+            }, 0);
+            setTimeout(() => {
+                text.textContent = "Blogger";
+            }, 4000);
+            setTimeout(() => {
+                text.textContent = "YouTuber";
+            }, 8000); //1s = 1000 milliseconds
+        }
 
+        textLoad();
+        setInterval(textLoad, 12000);
