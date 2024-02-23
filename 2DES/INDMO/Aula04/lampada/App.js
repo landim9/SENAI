d087c884f6141320b8cf6538e9daa6f98bf008ba
landim9 <landimr9des@gmail.com>
2024-02-23 11:26:27 -0300

    senai

--- 2DES/INDMO/Aula04/lampada/App.js
+++ 2DES/INDMO/Aula04/lampada/App.js
@@ -1,36 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, View, Switch } from 'react-native';
+import { ImageBackground, StyleSheet, View, Switch, Image } from 'react-native';
 
-const icon = require('./assets/icon.png');
+const icon = require('./assets/logo.png');
+const image = require('./assets/forest.jpg');
 
 export default function App() {
-  const [habilitado, setHabilidade] = new useState(false);
+  const [habilitado, setHabilitado] = new useState(false);
+
+  
 
   const habilitar = () => {
-    setHabilidade(habilitado);
-  };
+    setHabilitado(!habilitado);
+  }
+
   return (
     <View style={styles.container}>
+      <ImageBackground source={image} style={styles.image}></ImageBackground>
       <Image
-      source={icon}
-      style={styles.icon}
+        source={icon}
+        style={styles.icon}
+      />
+      <Switch
+        value={habilitado}
+        onValueChange={habilitar}
+        style={styles.switch}
       />
 
-      <Switch 
-      value={habilitado}
-      onvalueChange={habilitar}
+      <Image
+        source={{
+          uri: (habilitado)
+            ? "assets/MewTwo.png"
+            : "assets/Mew.png"
+        }}
+        style={styles.lampada}
       />
 
-      <image 
-      source={{
-        uri: (habilitado)
-
-       ? "./assets/lampadaon.png"
-       : "./assets/lampadaoff.png"
-      }}
-      />
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -38,17 +42,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    background: '#0d3b66',
     alignItems: 'center',
     justifyContent: 'center',
   },
   icon: {
-    width: '68px',
-    height: '68px',
+    width: '380px',
+    height: '150px',
+    display: 'none',
   },
   lampada: {
-    width: '200px',
-    height: '200px',
+    width: '500px',
+    height: '500px',
+    margin: '60px',
+    resizeMode: 'contain',
+    position: 'fixed',
+    top: '20rem',
+    left: 'auto'
+  },
+  image: {
+    width: '100%',
+    height: '100vh',
+    resizeMode: 'cover',
+
+  }, 
+  style: {
+    position: 'fixed',
+    top: 0,
+    left: 'auto',
   },
 
+  switch: {
+    position: 'fixed',
+    top: '20rem',
+    right: 'auto',
+  }
 });
