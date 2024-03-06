3953a4a4640ac348db20317cb3c4c7ca3d39d7d2
landim9 <landimr9des@gmail.com>
2024-03-06 11:21:04 -0300

    senai

--- 2DES/PWBE/transportadora/api/src/connection/mysql.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'trasnportadora'
-});
-
-module.exports = {con}
--- 2DES/PWBE/transportadora/api/src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routes = express.routes();
-
-
-const cliente = require('./controllers/cliente');
-
-routes.get('/', (req, res) => {res.json("API Transportadora Passos Aguiar");});
-
-routes.get('/cliente', cliente.getCliente);
